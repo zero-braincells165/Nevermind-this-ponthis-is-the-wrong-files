f2f05a51c88bbde18f599db5aedc986889c92c3c
YuriLivia <143151116+zero-braincells165@users.noreply.github.com>
2024-02-22 11:58:48 +0800

    Update script3.js

--- script3.js
+++ script3.js
@@ -10,14 +10,10 @@ function runScript3() {
         var heightDisplay = heightInFeet + "'" + remainingInches + "\"";
 
         var weightInLbs = weightInKg * 2.20462;
-
+        
+        alert("Name: " + nickname + "\nHeight: " + heightDisplay + "\nWeight: " + weightInLbs.toFixed(3) + " lbs");
+    }else {
         console.log
-        {
-            alert("Name: " + nickname + "\nHeight: " + heightDisplay + "\nWeight: " + weightInLbs.toFixed(3) + " lbs");
-        }
-    } else {
-        console.log}
             alert("User does not wish to share his/her information.");
-                   }
-    }
+            }
 }
